975efa786520882326c04b4cfaae56da1b965c57
Hectore Arabia <htorex.33@gmail.com>
2022-12-09 16:33:14 -0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,53 @@
+# medidas de pantalla
+width = 500
+height = 700
+SIZE = (width, height)
+
+# Player
+
+with_player = 50
+height_player = 50
+
+SIZE_PLAYER = (with_player, height_player)
+
+# Bullet
+
+width_bullet = 25
+height_bullet = 25
+
+SIZE_BULLET = (width_bullet, height_bullet)
+
+# Enemy
+
+width_enemy = 50
+height_enemy = 50
+
+SIZE_ENEMY = (width_enemy, height_enemy)
+
+# Laterales
+
+width_lateral = 8
+height_lateral = 562
+
+LATERAL_SIZE = (width_lateral, height_lateral)
+
+# Superior e Inferior
+
+width_sup = 170
+height_sup = 8
+
+SUP_SIZE = (width_sup, height_sup)
+
+# Medidas cancha
+
+width_field = 422
+height_field = 553
+
+FIELD_ZISE = (width_field, height_field)
+
+# Colores
+GREEN = (1, 100, 1)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 50)
+YELLOW = (255, 255, 0)
